53bed99d298e3581e7b585ea78e4e0fac83e7b48
Septian Rifano <septianinet@gmail.com>
2023-07-27 16:53:28 +0700

    change pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   apps : [{
     name   : "itpro-whatsapp",
-    script : "npm run dev"
+    script : "npm run dev",
+    env_production: {
+        NODE_ENV: "production",
+        PORT: 14045
+     },
+     env_development: {
+        NODE_ENV: "development",
+        PORT: 3000
+     }
   }]
 }
